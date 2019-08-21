7a19cff02ca972d78783c2433f58a5dd442c056c
Hiroshi <lucas.hh@hotmail.com>
2019-08-20 22:30:34 -0300

    Modificando para o application funcionar
    ngynx da erro

--- application/app/main.py
+++ application/app/main.py
@@ -7,11 +7,6 @@ app = Eve(__name__, static_folder="static")
 
 app.config['SECRET_KEY'] = "test"
 
-
 @app.route('/')
 def index():
     return render_template('example.html')
-
-if __name__ == "__main__":
-    # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=8088)
