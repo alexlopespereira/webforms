43b99ff4f2d2b9443e27f5a42d17168ac171a79c
Hiroshi <lucas.hh@hotmail.com>
2019-09-20 11:19:42 -0300

    adicionar nova rota

--- application/app/main.py
+++ application/app/main.py
@@ -12,5 +12,13 @@ def index(form_name):
     form_name = form_name
     return render_template('index.html', form_name=form_name)
 
+
+@app.route('/send_form/<form_name>', methods=["POST"])
+def create_form(form_name):
+    print(request)
+    template = render_template('index.html', messages=['You were successfully'])
+    return template
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=8088)
