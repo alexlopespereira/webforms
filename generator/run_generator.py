10bda084c9a19b4a9fd9d91d0ad2fd4700796b83
Hiroshi <lucas.hh@hotmail.com>
2019-10-17 14:43:17 -0300

    create function for app and process

--- generator/run_generator.py
+++ generator/run_generator.py
@@ -59,7 +59,13 @@ def create_form(servico):
     files = {'file': open(APP_PATH + "/static/assets/"+servico+".form", 'rb')}
     data = {'deploymentKey': servico, 'deploymentName': servico}
     r = requests.post(url, files=files, data=data, auth=('admin', 'test'))
+    
+    create_process(v1, servico)
+    create_app(servico)
 
+    return template
+
+def create_process(v1, servico):
     url = 'http://flowable-all-in-one-app:8080/flowable-task/process-api/process-repository/deployments/'
     srcfile = APP_PATH + "/static/assets/{0}.bpmn20.xml".format(v1)
     destfile = APP_PATH + "/static/assets/{0}.bpmn20_{1}.xml".format(v1, servico)
@@ -69,12 +75,10 @@ def create_form(servico):
     r = requests.post(url, files=files, data=data, auth=('admin', 'test'))
     print("Creating process. Status: {0}".format(r.response))
 
+def create_app(servico):
     url = 'http://flowable-all-in-one-app:8080/flowable-task/app-api/app-repository/deployments/'
     files = {'file': open(APP_PATH + "/static/assets/App1.zip", 'rb')} #TODO: Alterar o arquivo App1.zip antes de enviar
     data = {'deploymentKey': servico, 'deploymentName': servico}
     r = requests.post(url, files=files, data=data, auth=('admin', 'test'))
-
-    return template
-
 
 app2.run(host="0.0.0.0", port=8089)
